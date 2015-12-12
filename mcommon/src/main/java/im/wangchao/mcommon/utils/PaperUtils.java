bffcabdc0d4be165ef43b9c8dfbd254a8825fe5d
wangchao <magician.of.technique@gmail.com>
2015-12-12 15:13:12 +0800

    (*) commit Paper

---
+++ mcommon/src/main/java/im/wangchao/mcommon/utils/PaperUtils.java
@@ -0,0 +1,14 @@
+package im.wangchao.mcommon.utils;
+
+/**
+ * <p>Description  : PaperUtils.</p>
+ * <p/>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 15/12/12.</p>
+ * <p>Time         : 下午3:06.</p>
+ */
+public class PaperUtils {
+    private PaperUtils(){}
+
+    //todo use paper
+}
