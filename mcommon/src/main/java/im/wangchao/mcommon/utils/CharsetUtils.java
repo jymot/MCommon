052aeabb7f0aaf0a1072ca21b511d12bf9ae5127
wangchao <magician.of.technique@gmail.com>
2015-12-17 08:54:47 +0800

    (*) commit charsetUtils, DiskLruCache

---
+++ mcommon/src/main/java/im/wangchao/mcommon/utils/CharsetUtils.java
@@ -0,0 +1,15 @@
+package im.wangchao.mcommon.utils;
+
+import java.nio.charset.Charset;
+
+/**
+ * <p>Description  : CharsetUtils.</p>
+ * <p/>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 15/12/17.</p>
+ * <p>Time         : 上午8:48.</p>
+ */
+public class CharsetUtils {
+    public static final Charset US_ASCII = Charset.forName("US-ASCII");
+    public static final Charset UTF_8 = Charset.forName("UTF-8");
+}
