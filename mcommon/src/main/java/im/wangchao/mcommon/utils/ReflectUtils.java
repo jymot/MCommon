9124fc2fffcd822030e28e863bf349480d7d1d59
wangchao <magician.of.technique@gmail.com>
2015-12-26 13:48:40 +0800

    (*) ReflectUtils init

---
+++ mcommon/src/main/java/im/wangchao/mcommon/utils/ReflectUtils.java
@@ -0,0 +1,14 @@
+package im.wangchao.mcommon.utils;
+
+/**
+ * <p>Description  : ReflectUtils.</p>
+ * <p/>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 15/12/26.</p>
+ * <p>Time         : 下午1:47.</p>
+ */
+public class ReflectUtils {
+    private ReflectUtils(){
+        throw new AssertionError();
+    }
+}
