aca08179937009fd276a8cdc66ad97120818d9c9
wangchao <magician.of.technique@gmail.com>
2016-08-25 08:00:05 +0800

    (*)commit Singleton

---
+++ mcommon/src/main/java/im/wangchao/mcommon/utils/Singleton.java
@@ -0,0 +1,26 @@
+package im.wangchao.mcommon.utils;
+
+/**
+ * <p>Description  : Singleton.</p>
+ * <p/>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 16/8/24.</p>
+ * <p>Time         : 下午5:45.</p>
+ */
+public abstract class Singleton<T> {
+    private T instance;
+
+    protected abstract T create();
+
+    public T get(){
+        if (instance == null){
+            synchronized (this){
+                if (instance == null){
+                    instance = create();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
