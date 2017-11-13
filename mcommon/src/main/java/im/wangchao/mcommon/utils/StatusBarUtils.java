2d387d21514d09f8632b159cfacbb081619cbabe
wangchao <magician.of.technique@gmail.com>
2017-11-13 17:04:13 +0800

    [*] StatusBarUtils add setNavigationBarColor(), getNavigationBarHeight() method

--- mcommon/src/main/java/im/wangchao/mcommon/utils/StatusBarUtils.java
+++ mcommon/src/main/java/im/wangchao/mcommon/utils/StatusBarUtils.java
@@ -37,6 +37,26 @@ public class StatusBarUtils {
     }
 
     /**
+     * 获取 NavigationBar 高度
+     */
+    public static int getNavigationBarHeight(Context context){
+        if (!isEnable()){
+            return 0;
+        }
+        final int resourceId = context.getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
+    }
+
+    /**
+     * 设置 NavigationBar 颜色
+     */
+    public static void setNavigationBarColor(Activity activity, @ColorInt int color){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            activity.getWindow().setNavigationBarColor(color);
+        }
+    }
+
+    /**
      * 设置 StatusBar 颜色
      */
     public static void setStatusBarColor(Activity activity, @ColorInt int color){
@@ -76,6 +96,21 @@ public class StatusBarUtils {
             if (kitkatStatusBar != null){
                 kitkatStatusBar.setVisibility(View.GONE);
             }
+        }
+    }
+
+    /**
+     * 设置透明
+     */
+    public static void setTransparentForWindow(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+            activity.getWindow()
+                    .getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            activity.getWindow()
+                    .setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
     }
 
